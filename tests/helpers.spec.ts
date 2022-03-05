66b8a28ac3142fc8abc1fce037eb75bcc3e2e250
J. de Freitas <juniormfreitas@gmail.com>
2022-03-05 09:56:33 -0600

    Add more tests to makeHttpResponsesDictionary

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -1,9 +1,58 @@
 import { makeHttpResponsesDictionary } from "../src/helpers";
+import { HttpResponseObject } from "../src/Interfaces";
+
+import { StatusCode } from "../src/StatusCode";
+import { StatusPhrase } from "../src/StatusPhrase";
+import { StatusDescription } from "../src/StatusDescription";
 
 describe("makeHttpResponsesDictionary tests", () => {
-  it("should create a dictionary with 54 objects", () => {
-    const dictionary = makeHttpResponsesDictionary();
+  const dictionary = makeHttpResponsesDictionary();
 
+  it("should create a dictionary with 55 objects", () => {
     expect(Object.entries(dictionary)).toHaveLength(55);
+  });
+
+  it("should assert content from dictionary for an informational response", () => {
+    expect(dictionary[StatusCode.CONTINUE]).toMatchObject<HttpResponseObject>({
+      code: StatusCode.CONTINUE,
+      phrase: StatusPhrase.CONTINUE,
+      description: StatusDescription.CONTINUE,
+    });
+  });
+
+  it("should assert content from dictionary for a successful response", () => {
+    expect(dictionary[StatusCode.CREATED]).toMatchObject<HttpResponseObject>({
+      code: StatusCode.CREATED,
+      phrase: StatusPhrase.CREATED,
+      description: StatusDescription.CREATED,
+    });
+  });
+
+  it("should assert content from dictionary for a redirection message", () => {
+    expect(
+      dictionary[StatusCode.MOVED_PERMANENTLY]
+    ).toMatchObject<HttpResponseObject>({
+      code: StatusCode.MOVED_PERMANENTLY,
+      phrase: StatusPhrase.MOVED_PERMANENTLY,
+      description: StatusDescription.MOVED_PERMANENTLY,
+    });
+  });
+
+  it("should assert content from dictionary for a client error response", () => {
+    expect(dictionary[StatusCode.NOT_FOUND]).toMatchObject<HttpResponseObject>({
+      code: StatusCode.NOT_FOUND,
+      phrase: StatusPhrase.NOT_FOUND,
+      description: StatusDescription.NOT_FOUND,
+    });
+  });
+
+  it("should assert content from dictionary for a server error responses", () => {
+    expect(
+      dictionary[StatusCode.INTERNAL_SERVER_ERROR]
+    ).toMatchObject<HttpResponseObject>({
+      code: StatusCode.INTERNAL_SERVER_ERROR,
+      phrase: StatusPhrase.INTERNAL_SERVER_ERROR,
+      description: StatusDescription.INTERNAL_SERVER_ERROR,
+    });
   });
 });
