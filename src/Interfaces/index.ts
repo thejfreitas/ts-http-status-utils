456ad980284381c31843672fc2dfbf2bec3de391
J. de Freitas <juniormfreitas@gmail.com>
2023-12-13 18:38:46 -0700

    Add docs

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -2,12 +2,18 @@ import { StatusCode } from "../StatusCode";
 import { StatusDescription } from "../StatusDescription";
 import { StatusPhrase } from "../StatusPhrase";
 
+/**
+ * Represents an HTTP response object.
+ */
 export interface HttpResponseObject {
   code: StatusCode;
   phrase: StatusPhrase;
   description: StatusDescription;
 }
 
+/**
+ * Represents a dictionary of HTTP response objects.
+ */
 export interface IHttpResponsesDictionary {
   [id: number]: HttpResponseObject;
 }
