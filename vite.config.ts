63ea858111416bf8e4dbd9b4b08c14643f481a96
J. de Freitas <juniormfreitas@gmail.com>
2023-12-13 18:47:59 -0700

    Fix vite config type errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { defineConfig as vitestConfig } from "vitest/config";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
+const testConfig = vitestConfig({
+  test: {
+    include: ["./tests/*.ts"],
+    globals: true,
+  },
+});
+
+const config = defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -11,8 +19,9 @@ export default defineConfig({
     },
   },
   plugins: [dts()],
-  test: {
-    include: ["./tests/*.ts"],
-    globals: true,
-  },
 });
+
+export default {
+  ...testConfig,
+  ...config,
+};
